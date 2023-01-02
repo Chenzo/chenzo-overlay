3356a7a06ab9d85696dee4877620bf7ccfc7423c
Chenzo <chenzorama@hotmail.com>
2023-01-02 16:11:54 -0500

    audio listener good

--- components/HeadShot.js
+++ components/HeadShot.js
@@ -1,5 +1,5 @@
 import styles from "./HeadShot.module.scss";
-
+import CircleAudioBars from "./CircleAudioBars";
 
 export default function HeadShot() {
 
@@ -8,20 +8,12 @@ export default function HeadShot() {
             <div className={`goldBG ${styles.streamName}`}>
                 <div class="windlass">Mr<span>.</span> Chenzo</div>
             </div>
-            <div class="audioContainer">
-                <canvas width="500" height="500" id="circle-canvas"></canvas>
-            </div>
-            <div class="audioContainer flipped">
-                <canvas width="500" height="500" id="circle-canvas-2"></canvas>
-            </div>
+            <CircleAudioBars/>
             <div className={`${styles.imageContainer} ${styles.circleContainer}`}>
                 <video width="250" autoPlay={true} muted={true} loop={true}>
                     <source src="https://chenzorama.com/overlay/video/chenzo_headshot.webm"
                         type="video/webm" />
                 </video>
-
-                <div id="skull_bg" class="skull_bg"></div>
-                <div id="skull_blk_bg" class="skull_blk_bg"></div>
             </div>
             
         </div>
