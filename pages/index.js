805e3c46c1afa32153afdf98eba8097cbfb41735
Chenzo <chenzorama@hotmail.com>
2023-01-02 15:18:23 -0500

    headshot in place

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 
 import Header from 'components/Header'
+import HeadShot from 'components/HeadShot'
 
 export default function Home() {
   return (
@@ -8,8 +9,9 @@ export default function Home() {
       <Head>
         <title>Chenzo&apos;s Overlay</title>
       </Head>
+      <Header/>
       <main>
-          <Header/>
+          <HeadShot/>
 
       </main>
     </>
