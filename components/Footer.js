869c25a541e108dc8b229a67d37c64af6921bd1f
Chenzo <chenzorama@hotmail.com>
2023-01-02 20:05:18 -0500

    should be solid

--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,12 @@
 import styles from "./Footer.module.scss";
 
+import LatestFollowers from "./LatestFollowers";
 
-export default function Footer() {
+export default function Footer({twitchAccessToken}) {
 
     return (
         <footer className={styles.chenzo_footer}>
-            <div id="latest_followers" className={`${styles.latest} windlass`}>
-                <div>Latest Followers: <div id="latestFollowers" className={styles.followers}> - </div></div>
-            </div>
+            <LatestFollowers twitchAccessToken={twitchAccessToken}/>
             <div id="chatSpace" class="tchat windlass">
                 <div id="phrase" class="aphrase"><span class="usr">Chenzo</span>: <span class="msg">HAhahaha this is just me saying stuff</span></div>
             </div>
--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,10 @@ import Header from 'components/Header'
 import HeadShot from 'components/HeadShot'
 import Footer from 'components/Footer'
 
-export default function Home() {
+export default function Home({twitchAccessToken}) {
+
   return (
+    
     <>
       <Head>
         <title>Chenzo&apos;s Overlay</title>
@@ -15,7 +17,21 @@ export default function Home() {
           <HeadShot/>
 
       </main>
-        <Footer/>
+        <Footer twitchAccessToken={twitchAccessToken}/>
     </>
   )
+}
+
+
+export async function getServerSideProps() {
+
+  //Get Twitch Access Token  
+  const fData = await fetch('https://id.twitch.tv/oauth2/token?client_id=' + process.env.NEXT_PUBLIC_CLIENT_ID + '&client_secret=' + process.env.NEXT_PUBLIC_CLIENT_SECRET + '&grant_type=client_credentials', {
+      method: 'POST',
+    })
+  const jsonData = await fData.json()
+  const twitchAccessToken = jsonData.access_token;  
+
+
+  return { props: { twitchAccessToken } }
 }
