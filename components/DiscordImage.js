ae989788df13f5d5796022bd2c81f00c619b3714
Chenzorama <chenzorama@hotmail.com>
2024-08-04 11:17:37 -0400

    new imagepush stuff

--- components/DiscordImage.js
+++ components/DiscordImage.js
@@ -7,15 +7,18 @@ export default function DiscordImage({pushedImage, setPushedImage, setCurrentAud
 
     const [postid, setPostid] = useState(null);
     const [isTenor, setIsTenor] = useState(false);  
+
+    const [caption, setCaption] = useState("Chenzo pushed this image posted by Binobo in #channel!!");
+
     useEffect(() => {
         if (!pushedImage) {
             return;
         }
-        console.log("!!!pushed image: ");
-        console.log(pushedImage);
+        /* console.log("!!!pushed image: ");
+        console.log(pushedImage); */
         const script = document.createElement('script');
         
-        if (pushedImage.url.includes("tenor")) {
+        if (pushedImage?.url?.includes("tenor")) {
             //get the last part of url after the last -
             setPostid(pushedImage.url.split("-").pop());
             setIsTenor(true);
@@ -29,12 +32,15 @@ export default function DiscordImage({pushedImage, setPushedImage, setCurrentAud
         }
 
 
+        const captionMarkUp = <><span>{pushedImage.pusher}</span> pushed this image posted by <span>{pushedImage.author}</span> in <span>{pushedImage.channel}</span></>
+        setCaption(captionMarkUp);
+
         console.log("setting audio to shutter");
         setCurrentAudio("shutter");
         const timer = setTimeout(() => {
             console.log("clearing pushed image");
             setPushedImage(null);
-        }, 17000);
+        }, 13000);
         /* if (isTenor) {
             return () => {
                 if (isTenor) {
@@ -43,6 +49,18 @@ export default function DiscordImage({pushedImage, setPushedImage, setCurrentAud
             }
         } */
     }, [pushedImage]);
+
+    /* useEffect(() => {
+        const fakeData = {
+            "event": "imagePush",
+            "url": "https://cdn.discordapp.com/attachments/176173885559930880/1269026918393712751/sad_pirate.gif?ex=66b08b2a&is=66af39aa&hm=59f4e9d85af147014228eabc2021895f580fd73754fb634cad595e12ad369beb&",
+            "author": "chenzo",
+            "channel": "robots",
+            "dateTime": "2024-08-02T20:19:54.778Z",
+            "pusher": "chenzo"
+        };
+        setPushedImage(fakeData);
+    }, []); */
 
     /* useEffect(() => {
         //load this script dynamically: https://tenor.com/embed.js
@@ -58,14 +76,14 @@ export default function DiscordImage({pushedImage, setPushedImage, setCurrentAud
 
     return (
         <div className={styles.DiscordImage}>
-            {!pushedImage && 
+            {pushedImage && 
                 <aside className={`${styles.imageAside} ${isTenor ? styles.tenor : null}`}>
-                    <img src="https://cdn.dribbble.com/users/185738/screenshots/2751203/cowboy.gif" className={styles.theImage} />
-                    {/* {!isTenor && <img src={pushedImage.url} className={styles.pushedImage} />}
-                    {isTenor && <div className="tenor-gif-embed" data-postid={postid} data-share-method="host" data-aspect-ratio="0.833333" data-width="100%"><a href={pushedImage.url}>Loading Image From tenor</a></div> } */}
+                    {/* <img src="https://cdn.dribbble.com/users/185738/screenshots/2751203/cowboy.gif" className={styles.theImage} /> */}
+                    {!isTenor && <img src={pushedImage.url} className={styles.pushedImage} />}
+                    {isTenor && <div className="tenor-gif-embed" data-postid={postid} data-share-method="host" data-width="120%"><a href={pushedImage.url}>Loading Image From tenor</a></div> } 
                     <div className={styles.titleCard}>
-                    <p>Chenzo pushed this image posted by Binobo in #channel!!</p>
-                    <img src="/images/tbanner.png" className={styles.imageBanner}/>
+                        <p className={`${styles.caption} windlass`}>{caption}</p>
+                        <img src="/images/tbanner.png" className={styles.imageBanner}/>
                     </div>
                 </aside>
             }
